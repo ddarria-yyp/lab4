7eb3b572890beab90f636468bd693c462bc80738
dtd.com <dtd.com>
2024-01-07 23:37:35 +0300

    initial commit

---
+++ BooksCatalog/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace BooksCatalog;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum Category
+{
+    [Description("Title")]
+    Title,
+    [Description("Author")]
+    Author,
+    [Description("ISBN")]
+    ISBN,
+    [Description("Keyword")]
+    Keyword
+}
---
+++ BooksCatalogWebApi/Controllers/BookCatalogController.cs
@@ -0,0 +1,64 @@
+﻿using BooksCatalog;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Mime;
+
+namespace BooksCatalogWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookCatalogController : ControllerBase
+    {
+        private BooksComponent _bookComponents;
+
+        [ActivatorUtilitiesConstructor]
+        public BookCatalogController(IConfiguration configuration)
+        {
+           _bookComponents = new BooksComponent(configuration);
+        }
+
+        /// <summary>
+        /// Добавление книги в каталог
+        ///<param name="title">Название</param>
+        ///<param name="author">Автор</param>
+        ///<param name="isbn">ISBN</param>
+        ///<param name="annotaion">Аннотация</param>
+        ///<param name="genre">Жанр</param>
+        ///<param name="date">Дата публикации</param>
+        /// </summary>
+        [HttpPost("Add book")]
+        [Produces(MediaTypeNames.Application.Json)]
+        public IActionResult AddBook(string title, string author, string isbn, string annotaion, DateTime date, string genre)
+        {
+            var book = new Book(title, author, isbn, annotaion, date, genre);
+            _bookComponents.AddBook(book);
+            return Ok();
+        }
+
+
+        /// <summary>
+        /// Поиск книг
+        /// </summary>
+        /// <param name="category">Поиск по</param>
+        /// <param name="keyWords">Параметры запроса</param>
+        /// <returns></returns>
+        [HttpGet("FindBooks")]
+        public IActionResult FindBooks(Category category, string keyWords)
+        {
+            IEnumerable<Book> books = _bookComponents.FindBooksByCategory(category, keyWords);
+            return Ok(books);
+        }
+
+        /// <summary>
+        ///Список всех книг
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("AllBooks")]
+        public IActionResult GetAllBooks()
+        {
+            IEnumerable<Book> books = _bookComponents.BookCatalog.Books;
+            return Ok(books);
+        }
+
+    }
+}
